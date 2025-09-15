8d8c2083813fd7ba783c6bde9d2d4937dee2fb71
Yulei Qiu <ray.yuleiqiu@gmail.com>
2025-09-15 18:43:46 +0100

    use: daily usage, dont mind these changes

--- scripts/eval/eval.sh
+++ scripts/eval/eval.sh
@@ -1,15 +1,13 @@
 
 python evaluate.py \
-    task=libero_object_grid_random \
-    checkpoint_path=./experiments/libero/LIBERO_OBJECT_GRID_RANDOM/act/debug/block_16/5000/stage_1/ \
+    task=libero_object_random \
+    checkpoint_path=./experiments/libero/mixed_datasets/500_1500/act/5000/run_000 \
     algo=act \
-    exp_name=debug \
-    variant_name=block_16 \
-    stage=1 \
+    exp_name=\'500_1500\' \
     training.use_tqdm=true \
     rollout.n_video=10 \
     rollout.num_parallel_envs=5 \
-    seed=5000
+    seed=6000
 
 # Note1: this will automatically load the latest checkpoint as per your exp_name, variant_name, algo, and stage.
 #        Else you can specify the checkpoint_path to load a specific checkpoint.
--- scripts/mixed/train_on_mixed.sh
+++ scripts/mixed/train_on_mixed.sh
@@ -4,7 +4,7 @@ python train_concat_datasets.py --config-name=train_mixed.yaml \
     +task.dataset_grid.n_demos=0 \
     +task.dataset_random.n_demos=2000 \
     +task.dataset_random.task_ids='[0]' \
-    algo=act \
+    algo=diffusion_policy \
     exp_name=\'0_2000\' \
     training.use_tqdm=true \
     training.save_all_checkpoints=true \
@@ -14,7 +14,7 @@ python train_concat_datasets.py --config-name=train_mixed.yaml \
     train_dataloader.num_workers=6 \
     make_unique_experiment_dir=true \
     algo.skill_block_size=16 \
-    logging.group=act \
+    logging.group=dp \
     rollout.enabled=true \
     seed=10000
 
