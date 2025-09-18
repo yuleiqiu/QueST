474b33b70379ac287448945719de90484a192890
Yulei Qiu <ray.yuleiqiu@gmail.com>
2025-09-18 15:07:39 +0100

    feat: update evaluation script for improved task handling and configuration

--- scripts/eval/eval.sh
+++ scripts/eval/eval.sh
@@ -1,14 +1,14 @@
-
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
 # This script is used to evaluate the DP model trained on mixed datasets
 python evaluate_single_task_hydra.py --config-name=evaluate_single_task_custom -m \
-    seed="range(6000,6003)" \
-    checkpoint_dir=./experiments/libero/mixed_datasets/1000_1000/diffusion_policy/5000/run_000 \
-    task.task_id=0 \
-    algo=diffusion_policy \
+    seed=6000,6001,6002 \
+    exp_name=\'2000_0\' \
+    checkpoint_dir=experiments/libero/LIBERO_OBJECT_GRID/act/debug/block_16/5000/run_002 \
+    task.task_id=[0] \
+    algo=act \
     eval_all_checkpoints=false \
-    rollout.num_parallel_envs=5 \
+    rollout.num_parallel_envs=50 \
     rollout.n_video=50 \
     rollout.rollouts_per_env=50
